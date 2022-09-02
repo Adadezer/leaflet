7e572b1c7dc4b73eff5105a227f595f499725f30
Adadezer <adadezer@gmail.com>
2022-09-02 16:48:52 -0300

    feat: :zap: adição de vários marcadores e icones personalizados
    guia de mapa interativo com Leaflet e OpenStreetMap, artigo asmaloney

--- asmaloney/maps/leaf-demo.js
+++ asmaloney/maps/leaf-demo.js
@@ -11,8 +11,19 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   subdomains: ['a','b','c']
 }).addTo( map );
 
-for ( var i=0; i < markers.length; ++i ) {
-  L.marker( [markers[i].lat, markers[i].lng] )
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+});
+
+for ( var i=0; i < markers.length; ++i ) 
+{
+  L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
     .bindPopup( '<a href="' + markers[i].url + '" target="_blank" rel="noopener">' + markers[i].name + '</a>' )
     .addTo( map );
 }
